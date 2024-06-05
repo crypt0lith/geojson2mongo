707fc51f7a9d225901a9a807687faee18c7b0ce8
cryptolith <ryoheiuchida25@gmail.com>
2024-06-05 15:11:53 -0500

    Merge remote-tracking branch 'origin/master'

--- post_install.py
+++ post_install.py
@@ -14,15 +14,15 @@ def extract_zip(archive_path, extract_dir):
 
 def main():
     archives = [{
-        "path": os.path.join(os.path.dirname(__file__), 'geojson2mongo/archives', 'src_json.zip'),
-        "extract_to": os.path.join(os.path.dirname(__file__), 'geojson2mongo/json_data')
+        'path': '_src.zip',
+        'extract_to': os.path.join(os.path.dirname(__file__), 'geojson2mongo/json_data')
     }, {
-        "path": os.path.join(os.path.dirname(__file__), 'geojson2mongo/archives', 'rwanda-2015-geojson.zip'),
-        "extract_to": os.path.join(os.path.dirname(__file__), 'geojson2mongo/json_data')
+        'path': '_rwanda2015.zip',
+        'extract_to': os.path.join(os.path.dirname(__file__), 'geojson2mongo/json_data')
     }]
     for archive in archives:
-        extract_zip(archive["path"], archive["extract_to"])
+        extract_zip(archive['path'], archive['extract_to'])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
